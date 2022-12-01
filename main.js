56833446edbb93ae34300a9cb9eb60dfb81b2074
hiimericson <105856244+hiimericson@users.noreply.github.com>
2022-12-01 23:18:14 +0800

    Local storage

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+localStorage.getItem("input");
 window.addEventListener('load', ()=>{
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
